9ddf58a02dd1f05be0944c2efeaa7957f693ffda
Martin Ruthaivilavan <mmvilavan@gmail.com>
2023-06-20 09:34:02 -0500

    faq accordion component

--- src/views/Resources/Resources.jsx
+++ src/views/Resources/Resources.jsx
@@ -1,24 +1,78 @@
 import React from 'react'
-
+import FaqCompetitive from '../../components/FaqCompetitive/FaqCompetitive'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../container/Footer/Footer'
 import './Resources.css'
+import { images } from "../../constants";
+import { useState } from "react";
 
 const Resources = () => {
- 
+  const [faqC, setFaqC] = useState([
+    {
+      id: 1,
+      question: 'Lorem ipsum dolor sit amet?',
+      answer: 'Lorem, ipsum.',
+      open: false,
+    },
+    {
+      id:2,
+      question: 'Lorem, ipsum dolor sit amet consectetur adipisicing?',
+      answer: 'Lorem, ipsum consectetur.',
+      open: false,
+    },
+    {
+      id:3,
+      question: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit?',
+      answer: 'Lorem, ipsum sit amet.',
+      open: false,
+    },
+  ]);
+
+
+  const toggleFaq = index => {
+    setFaqC(faqC.map((faq, i) => {
+      if (i === index) {
+        faq.open = !faq.open
+      } else {
+        faq.open = false;
+      }
+
+      return faq;
+    }))
+  }
+
+
   return (
-    <div id="resources">
+    <div className="app__resources" id="resources">
       <Navbar />
       <div className="app__resources-title flex__center section_padding">
         <h1 className="headtext__playfair text__resources">Resources</h1>
       </div>
       <div className="app__resources-content section__padding" >
-        <div class="app__resources-competitive">
-          <h2 className="app__resources-title">Competitive Questions</h2>
-          </div>
+        <div className="app__resources-competitive ">
+          <h2 className="app__resources-title">Competitive FAQ</h2>
+          <img src={images.pitch} alt="about_pitch" className="pitch__img"/>
+        </div>
+        <div className="app__resources-faq app__faq ">
+          {faqC.map((faq, i) => (
+            <FaqCompetitive faq={faq} index={i} toggleFaq={toggleFaq} />))}
+        </div>
       </div>
-      <Footer/>
+      <div className="app__resources-content section__padding" >
+        <div className="app__resources-competitive ">
+          <h2 className="app__resources-title"> Recreation FAQ</h2>
+          <img src={images.pitch} alt="about_pitch" className="pitch__img"/>
+        </div>
+        <div className="app__resources-faq app__faq ">
+          {faqC.map((faq, i) => (
+            <FaqCompetitive faq={faq} index={i} toggleFaq={toggleFaq} />))}
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
+
 
   )
 }
